0170f7f90ac31e611d3a748b5dac9ab8f67cce40
thomasptharakan <thomasptharakan@gmail.com>
2023-01-17 19:40:20 +0000

    Fixed functionality to display questions added

--- js/index.js
+++ js/index.js
@@ -4,9 +4,13 @@ var startButton = document.querySelector("#start");
 var choices = document.querySelector("#choices");
 var timeSpan = document.querySelector("#time");
 var endScreen = document.querySelector("#end-screen");
-var currentQuestionIndex = 0;
+var answerBarLine = document.querySelector('#answerBarLine')
+var answerStatus = document.querySelector('#answerStatus');
 
-var timer=0;
+var currentQuestionIndex = 0;
+var timer=80;
+var score=0;
+
 
 function displayQuestions(currentQuestion){
     alert('in here');
@@ -33,14 +37,27 @@ function getQuestions(){
 }
 
 function handleClick(){
+    //Get Current choice
     var choice = this.textContent;
-    var answer= questions[currentQuestionIndex].answer;
+    
+    //Get Answer
+    var answer = questions[currentQuestionIndex].answer;
+    //Increment Answer
     currentQuestionIndex++;
+    
+    //display answer status
+    answerBarLine.removeAttribute('class');
+    answerStatus.removeAttribute('class');
+
+    //Check Answer
     if (choice===answer){
-        return;
+        //Increment Score and return to next question
+        answerStatus.textContent = 'Correct';
     }else{
-        timer = timer - 10;
+        timer = timer - 20;
+        answerStatus.textContent = 'Wrong';
     }
+    
     if (currentQuestionIndex<(questions.length)){
         getQuestions();
     }else{
--- js/questions.js
+++ js/questions.js
@@ -1,23 +1,35 @@
 var questions =  [
     {
-        question: "Whats is 1ffffsdfsdfsdgasdfgads",
-        choices : [ 1, 2, 3,4],
-        answer : 4
+        question: "What are the data types not supported by JavaScript ?",
+        choices : [ "1. Undefined", 
+                    "2. Null", 
+                    "3. Boolean",
+                    "4. Objectify"],
+        answer : "4. Objectify"
     },
     {
-        question: "1ffffsdfsdfsdgasdfgadssdfasdgfdsgsfgfgsdfgs",
-        choices : [ 1, 2, 3,4],
-        answer : 4
+        question: "What are the features of JavaScript ?",
+        choices : [ "1. It is a lightweight, interpreted programming language.", 
+                    "2. Its has jumping beans in them", 
+                    "3. It is a low level scripting language",
+                    "4. It is machine code."],
+        answer : "1. It is a lightweight, interpreted programming language."
     },
     {
-        question: "Whats is 1",
-        choices : [ 1, 2, 3,4],
-        answer : 4
+        question: "Whats does JS stand for ?",
+        choices : [ "1. Just Simple", 
+                    "2. Java Scripting",
+                    "3. Java Script", 
+                    "4. Jester Scieper"],
+        answer : "2. Java Script"
     },
     {
-        question: "Whats is 1",
-        choices : [ 1, 2, 3,4],
-        answer : 4
+        question: "Is JavaScript a case-sensitive language",
+        choices : [ "1. Yes", 
+                    "2. No",
+                    "3. Maybe",
+                    "4. All of the Above"],
+        answer : "1. Yes"
     }
     
 
